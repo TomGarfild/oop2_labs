3e9d95fecdc8a314f162911c2c5a9467a7177813
TomGarfild <safronuyk.allex73@gmail.com>
2021-09-26 19:14:15 +0300

    OOP-L14. Add unit tests for stack.

---
+++ Lab_1/TestProject/Stack/StackPopTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Lab_1;
+using NUnit.Framework;
+
+namespace TestProject.Stack
+{
+    public class StackPopTest
+    {
+        [Test]
+        public void StackPopOk()
+        {
+            // Arrange
+            var stack = new Stack<int>(10);
+
+            // Act
+            for (var i = 0; i < 10; i++)
+            {
+                stack.Push(i);
+            }
+
+            // Assert
+            Assert.AreEqual(10, stack.Count);
+            if (!stack.TryPop(out var value))
+            {
+                Assert.Fail("Stack should not be empty!");
+            }
+
+            Assert.AreEqual(9, value);
+        }
+
+        [Test]
+        public void StackPopThrow()
+        {
+            // Arrange
+            var stack = new Stack<int>();
+
+            // Act && Assert
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+    }
+}
---
+++ Lab_1/TestProject/Stack/StackPushTest.cs
@@ -0,0 +1,40 @@
+using System;
+using Lab_1;
+using NUnit.Framework;
+
+namespace TestProject.Stack
+{
+    public class StackPushTest
+    {
+        [Test]
+        public void StackPushOk()
+        {
+            // Arrange
+            var stack = new Stack<int>(5);
+
+            // Act
+            for (var i = 0; i < 10; i++)
+            {
+                stack.PushWithResize(i);
+            }
+
+            // Assert
+            Assert.AreEqual(10, stack.Count);
+            if (!stack.TryPeek(out var value))
+            {
+                Assert.Fail("Stack is empty!");
+            }
+            Assert.AreEqual(9, value);
+        }
+
+        [Test]
+        public void StackPushThrow()
+        {
+            // Arrange
+            var stack = new Stack<int>();
+
+            // Act && Assert
+            Assert.Throws<InvalidOperationException>(() => stack.Push(0));
+        }
+    }
+}
