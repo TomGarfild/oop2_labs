ddb8ad98920815d59a3cb966793ae7ac3b79769f
TomGarfild <safronuyk.allex73@gmail.com>
2022-06-09 09:55:05 +0300

    Add usersmanager

---
+++ CryptoHelper/Kernel.Data/Managers/UsersManager.cs
@@ -0,0 +1,18 @@
+﻿using Kernel.Data.Entities;
+
+namespace Kernel.Data.Managers;
+
+public class UsersManager
+{
+    private readonly DataDbContext _dbContext;
+
+    public UsersManager(DataDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<UserData> Create()
+    {
+        return null;
+    }
+}
