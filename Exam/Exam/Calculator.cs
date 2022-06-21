dc124279b34844648685fd58ab3395946fcb7e8a
TomGarfild <safronuyk.allex73@gmail.com>
2022-06-21 10:44:12 +0300

    Exam

---
+++ Exam/Exam/Calculator.cs
@@ -0,0 +1,39 @@
+﻿namespace Exam
+{
+    //LSP
+    public abstract class Calculator
+    {
+        protected readonly int[] Numbers;
+
+        protected Calculator(int[] numbers)
+        {
+            Numbers = numbers;
+        }
+
+        public abstract int Calculate();
+    }
+
+    public class SumCalculator : Calculator
+    {
+        public SumCalculator(int[] numbers) : base(numbers)
+        {
+        }
+
+        public override int Calculate()
+        {
+            return Numbers.Sum();
+        }
+    }
+
+    public class SumEvenCalculator : Calculator
+    {
+        public SumEvenCalculator(int[] numbers) : base(numbers)
+        {
+        }
+
+        public override int Calculate()
+        {
+            return Numbers.Where(n => n % 2 == 0).Sum();
+        }
+    }
+}
